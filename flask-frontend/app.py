32da64f6c9d351018baf19fd70232a6897b9d93a
Daniel Avila <daigo.tnt@gmail.com>
2023-11-19 12:07:19 -0300

    docker compose mongo

--- flask-frontend/app.py
+++ flask-frontend/app.py
@@ -1,31 +1,30 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Replace this URL with your Go GraphQL server endpoint
-GRAPHQL_URL = "http://localhost:8000/graphql"
+    # Replace this URL with your Go GraphQL server endpoint
+    GRAPHQL_URL = "http://backend:8000/graphql"
 
-def fetch_images():
-    query = '''
-    {
-        images {
-            ID
-            Link
+    def fetch_images():
+        query = '''
+        {
+            images {
+                ID
+                Link
+            }
         }
-    }
-    '''
+        '''
 
-    response = requests.post(GRAPHQL_URL, json={'query': query})
-    data = response.json()
-    return data['data']['images']
+        response = requests.post(GRAPHQL_URL, json={'query': query})
+        data = response.json()
+        return data['data']['images']
 
-@app.route('/')
-def index():
-    images = fetch_images()
+    @app.route('/')
+    def index():
+        images = fetch_images()
+        return render_template('index.html', images=images)
 
-    return render_template('index.html', images=images)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
 
